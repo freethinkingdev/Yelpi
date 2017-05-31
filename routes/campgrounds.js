42df75c9cbb9eb3827d45b17222a221f62e6e025
Marcian Wuu <marcianwuu@gmail.com>
2017-05-31 17:01:17 +0100

    Added route and view for campgrounds.

---
+++ routes/campgrounds.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var campgroundsArray = [
+  {name: "Dakota Camp", img: 'https://s-media-cache-ak0.pinimg.com/736x/ae/d5/d2/aed5d2f14c3df4caf54bc101a53cdbd5.jpg'},
+  {name: "ScoCamp", img: 'http://www.camping-vagues-oceanes.co.uk/sites/default/files/styles/img_250/public/2016_camping_vilanova_park_galerie_espace_aquatique_3.jpg?itok=isOZ26ED'},
+  {name: "Montana Grill Camp", img: 'https://3.imimg.com/data3/FB/AR/MY-10292644/jungle-camping-250x250.jpg'},
+  {name: "Baidu Night Camp", img: 'http://www.caerddaniel.co.uk/contentimages/0/0/camping%202.jpg'},
+  {name: "Studenti Campus", img: 'http://www.trawsdir.co.uk/contentimages/0/0/camping%20touring%206.jpg'},
+  {name: "Campus Otinelli", img: 'http://www.trawsdir.co.uk/contentimages/0/0/camping%20touring%201.jpg'}
+
+];
+
+/* GET campgrounds page. */
+router.get('/', function(req, res, next) {
+  res.render('campgrounds', {
+    title: 'Camp Grounds',
+    author: 'Pimpek',
+    campgrounds: campgroundsArray
+  });
+});
+
+module.exports = router;
