0918745699b54d65f14650333ce0e54f05d01627
Marcian Wuu <marcianwuu@gmail.com>
2017-05-31 12:40:25 +0100

    Added partials folder with header and footer template. Updated error view.

---
+++ routes/error.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/*', function(req, res, next) {
+  res.render('error', {
+    message: "One Big Error"
+  });
+});
+
+module.exports = router;
