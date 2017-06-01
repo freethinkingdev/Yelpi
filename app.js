e53b4d5dbf2c6246c914cc10f573e74ba6b4dc3c
Marcian Wuu <marcianwuu@gmail.com>
2017-06-01 13:23:39 +0100

    Added new view - add new camp page camping site and updated app.js and partial header to reflect the change.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var aboutus = require('./routes/aboutus');
 var users = require('./routes/users');
 var contact = require('./routes/contact');
 var campgrounds = require('./routes/campgrounds');
+var campgroundsadd = require('./routes/campgroundsadd');
 
 var app = express();
 
@@ -33,6 +34,7 @@ app.use('/aboutus', aboutus);
 app.use('/users', users);
 app.use('/contact', contact);
 app.use('/campgrounds', campgrounds);
+app.use('/campgroundsadd', campgroundsadd);
 app.use('/*', error);
 
 // catch 404 and forward to error handler
