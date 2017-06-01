c8ec19df43461891cb790d7eed9b2adfc68f11af
Marcian Wuu <marcianwuu@gmail.com>
2017-06-01 13:22:22 +0100

    Removed asteriks from the path.

--- routes/error.js
+++ routes/error.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* Error for the page. */
-router.get('/*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('error', {
       title: 'Error',
       message: "One Big Error. It seems we have 404 which is page not found. Too bad...",
