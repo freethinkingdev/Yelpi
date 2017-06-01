5f45f9141c63828a7e7ef8f0dd1756efaf4d96f3
Marcian Wuu <marcianwuu@gmail.com>
2017-06-01 16:24:04 +0100

    Updated route for adding new camp sites.

--- routes/campgroundsadd.js
+++ routes/campgroundsadd.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 /* GET new campgrounds page. */
 router.get('/', function(req, res, next) {
   res.render('campgroundsadd', {
     title: 'Add New Camp Ground',
-    author: 'Pimpek Maximus',
-    campgrounds: campgroundsArray
+    author: 'Pimpek Maximus'
   });
+
 });
 
 module.exports = router;
