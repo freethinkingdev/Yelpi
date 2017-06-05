753d74bcfe2426eca532cbddeaee1f7fc7d1c4cb
marcin <emwu83@gmail.com>
2017-06-05 11:27:11 +0100

    Updated view and route for the details about any given campground.

---
+++ routes/campgroundsdetail.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET campgrounds page. */
+router.get('/', function(req, res, next) {
+
+    var objectId = req.baseUrl.split("/").slice(-1)[0];
+    var objectArray = [];
+    Campsite.findById(objectId, function (err, result) {
+        if(err) {
+            console.log(err);
+        } else {
+            // console.log(res);
+            res.render('campgroundsdetail', {
+                title: 'Camp Grounds Detail',
+                author: 'Pimpek',
+                objectArray: result
+            });
+        }
+    });
+
+
+    // console.log("This is campsite details route");
+
+});
+
+
+module.exports = router;
