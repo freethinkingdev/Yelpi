ac380b3281a9ced680768cc50cd32534ad60110d
marsinwu <w.marcin@yahoo.co.uk>
2017-06-12 12:50:28 +0100

    Added separate models and updated files accordingly. Updated app.js formatting.

--- models/campsites.js
+++ models/campsites.js
@@ -2,11 +2,16 @@
  * Created by marsin on 10/06/17.
  */
 var mongoose = require('mongoose');
+var Comment = require('../models/comment');
 
 var campsitesSchema = new mongoose.Schema({
     name: String,
     description: String,
-    img: String
+    img: String,
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
 });
 
 var Campsite = mongoose.model("Campsite", campsitesSchema);
--- routes/campgroundsdetail.js
+++ routes/campgroundsdetail.js
@@ -6,6 +6,7 @@ var Campsite = require('../models/campsites');
 router.get('/', function (req, res, next) {
     // var objectId = req.baseUrl.split("/").slice(-1)[0];
     var objectId = req.params.id;
+    /*
     Campsite.findById(objectId, function (err, result) {
         if (err) {
             console.log(err);
@@ -18,7 +19,18 @@ router.get('/', function (req, res, next) {
             });
         }
     });
-
+     */
+    Campsite.findOne({_id: objectId}).populate('comments').exec(function (err, campsiteData) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgroundsdetail', {
+                title: 'Camp Grounds Detail',
+                author: 'Pimpek',
+                objectArray: campsiteData
+            });
+        }
+    });
 });
 
 
