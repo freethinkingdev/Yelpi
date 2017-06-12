92fe658051dcfbb25ed7cef856d349e40f5a0a7c
marsinwu <w.marcin@yahoo.co.uk>
2017-06-12 12:53:20 +0100

    Working with js code in order to add some animations.

--- public/javascripts/mainJS.js
+++ public/javascripts/mainJS.js
@@ -2,33 +2,14 @@
  * Created by marsin on 03/06/17.
  */
 
-var mongoose = require("mongoose");
+$(document).ready(function () {
 
-//connect to db
-mongoose.connect("mongodb://localhost/demo");
-// cat model
-var kotSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-});
+    var addNewCommentForm = document.getElementById("#addNewComment");
+    var revealCommentFormButton = document.getElementById("#revealAddNewCommentForm");
 
-var Kicia = mongoose.model("Kicia", kotSchema);
-//add cat to db
-for (var i =1;i<6;i+=2 ) {
-    Kicia.create({name:"Kcia",age:i},function (err,res) {
-        if(err){console.log(err);}else{console.log(res);}
+    revealCommentFormButton.on("click", function () {
+        alert('works');
     });
 
-}
-//retrive cat from db
-
-/*
-Cat.find({}, function(err,res){
-    if(err){
-        console.log(err);
-    }else{
-        res.forEach(function (cat) {
-            console.log("Kitten: " +cat);
-        });
-    }
-});*/
+    addNewCommentForm.slideDown("slow");
+});
