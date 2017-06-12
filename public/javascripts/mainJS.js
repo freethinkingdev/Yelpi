af67a093dabf68580eb8aa6d08366d990933783b
marsinwu <w.marcin@yahoo.co.uk>
2017-06-12 22:20:37 +0100

    Added sliding functionality for the form using jquery.

--- public/javascripts/mainJS.js
+++ public/javascripts/mainJS.js
@@ -2,14 +2,18 @@
  * Created by marsin on 03/06/17.
  */
 
+
 $(document).ready(function () {
-
-    var addNewCommentForm = document.getElementById("#addNewComment");
-    var revealCommentFormButton = document.getElementById("#revealAddNewCommentForm");
-
-    revealCommentFormButton.on("click", function () {
-        alert('works');
+    var showCommentForm = $("#showCommentAddForm");
+    var addCommentForm = $("#newCommentForm");
+    addCommentForm.slideUp("slow", function () {
+        $("#commentsIcon").removeClass("fa-arrow-down").addClass("fa-arrow-right");
+        //$("#commentsIcon").addClass("fa-arrow-right");
     });
 
-    addNewCommentForm.slideDown("slow");
+    showCommentForm.on("click", function () {
+        $(".row #newCommentForm").slideToggle();
+        //$("#commentsIcon").removeClass("fa-arrow-right").addClass("fa-arrow-down");
+        $("#commentsIcon").toggleClass("fa-arrow-down").toggleClass("fa-arrow-right");
+    });
 });
