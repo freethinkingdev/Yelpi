e0d531bed75bf698ae872112f8637667d072ec1b
marsinwu <w.marcin@yahoo.co.uk>
2017-06-13 09:32:23 +0100

    Testing git merging.

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -2,3 +2,21 @@
  * Created by marsin on 13/06/17.
  */
 alert('But are  sure it is alive????');
+
+
+$(document).ready(function () {
+    var showCommentForm = $("#showCommentAddForm");
+    var addCommentForm = $("#newCommentForm");
+    addCommentForm.slideUp("slow", function () {
+        $("#commentsIcon").removeClass("fa-arrow-down").addClass("fa-arrow-right");
+        //$("#commentsIcon").addClass("fa-arrow-right");
+    });
+
+    showCommentForm.on("click", function () {
+        alert('It is alive');
+        alert('It is alive');
+        alert('It is alive');
+        alert('It is alive');
+        alert('It is alive');
+    });
+});
