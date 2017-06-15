45aad05872f35b926a4d6798ae5cff026aaca265
marsinwu <w.marcin@yahoo.co.uk>
2017-06-15 16:07:34 +0100

    Changed comments form naming conventions. Changed other files to reflect refactoring.

--- routes/comments/new.js
+++ routes/comments/new.js
@@ -6,12 +6,12 @@ var Comment = require('.././/../models/comment');
 /* POST save new campground and show camp grounds list. */
 router.post('/', function (req, res, next) {
     /*Checking to see if data has been received*/
-    if (req.body.commentAuthor && req.body.commentBody && req.body.siteId) {
-        var comAuthor = req.body.commentAuthor;
-        var comBody = req.body.commentBody;
+    if (req.body.comment && req.body.comment != null) {
+
         /* Getting the id of the object that is the campsite */
         var objectId = req.params.id;
-
+        /* Getting comment data from grouped form data */
+        var commentObject = req.body.comment;
 
         Campsite.findById(objectId, function (err, foundCampsite) {
             if (err) {
@@ -19,10 +19,7 @@ router.post('/', function (req, res, next) {
                 console.log(err);
             } else {
                 /* No errors */
-                Comment.create({
-                    text: comBody,
-                    author: comAuthor
-                }, function (err, newComment) {
+                Comment.create(commentObject, function (err, newComment) {
                     if (err) {
                         /* If there is error */
                         console.log(err);
